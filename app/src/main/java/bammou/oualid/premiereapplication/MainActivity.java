7a45278f02e0989b1c2cf3dcae05fd78a9b5ddf3
oualid bammou <bammou.ceo@gmail.com>
2022-10-18 21:21:56 +0200

    Événements associés aux objets graphiques d’une vue : Affichage d'une fenetre de confirmation aprés le click sur le button de validation

--- app/src/main/java/bammou/oualid/premiereapplication/MainActivity.java
+++ app/src/main/java/bammou/oualid/premiereapplication/MainActivity.java
@@ -2,7 +2,11 @@ package bammou.oualid.premiereapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Créer une AlertDialog (fenêtre de dialogue) pour confirmer ou annuler
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setMessage(R.string.confirmation_message) // voir la valeur dans strings.xml
+        .setTitle(R.string.confirmation_titre) // voir la valeur dans strings.xml
+        .setPositiveButton(R.string.confirmer, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // CONFIRMER
+            }
+        })
+        .setNegativeButton(R.string.annuler, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // ANNULER
+            }
+        });
+
+        AlertDialog fenetreDeConfirmation = alertBuilder.create();
+
+        // Retrouver le button de validation :
+        Button btn_valider = (Button) findViewById(R.id.buttonValider);
+        // Evenement onClick pour le button de validation :
+        btn_valider.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // Afficher la fenetre qu'on a créé aprés le click :
+                fenetreDeConfirmation.show();
+            }
+        });
     }
 }
